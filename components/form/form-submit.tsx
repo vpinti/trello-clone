200e8ee8d15c8e28193adf0d4a5ce8c4360f3af3
vpinti <vittorio.pinti@gmail.com>
2024-03-05 22:53:16 +0100

    form reusable component

---
+++ components/form/form-submit.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+interface FormSubmitProps {
+    children: React.ReactNode;
+    disabled?: boolean;
+    className?: string;
+    variant?:
+        | "default"
+        | "destructive"
+        | "outline"
+        | "secondary"
+        | "ghost"
+        | "link"
+        | "primary";
+}
+
+const FormSubmit: React.FC<FormSubmitProps> = ({
+    children,
+    disabled,
+    className,
+    variant,
+}) => {
+    const { pending } = useFormStatus();
+
+    return (
+        <Button
+            disabled={pending || disabled}
+            type="submit"
+            variant={variant}
+            size="sm"
+            className={cn(className)}
+        >
+            {children}
+        </Button>
+    );
+};
+
+export default FormSubmit;
