da8e5c833a15c2306cd5172f514d860c37d74eb7
vpinti <vittorio.pinti@gmail.com>
2024-03-12 23:02:36 +0100

    board page + server actions update/delete board

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,16 +1,23 @@
-import { OrgControl } from "./_components/org-control"
+import { startCase } from "lodash";
+import { auth } from "@clerk/nextjs";
 
-const OrganizationIdayout = ({
-    children
-}: {
-    children: React.ReactNode
-}) => {
-  return (
-    <>
-        <OrgControl />
-        {children}
-    </>
-  )
+import { OrgControl } from "./_components/org-control";
+
+export async function generateMetadata() {
+    const { orgSlug } = auth();
+
+    return {
+        title: startCase(orgSlug || "Organization"),
+    };
 }
 
-export default OrganizationIdayout
+const OrganizationIdayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <>
+            <OrgControl />
+            {children}
+        </>
+    );
+};
+
+export default OrganizationIdayout;
